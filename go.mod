fd596b3d4caabe1611571d709ee7b8f48c9a6b72
Karl <karl.hepworth@finance.com>
2021-01-04 11:11:53 +1100

    namespace create/delete & reporting
    Signed-off-by: Karl <karl.hepworth@finance.com>

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/fubarhouse/fubarhouse.github.io
 go 1.15
 
 require (
-	k8s.io/api v0.19.0 // indirect
-	k8s.io/apimachinery v0.20.1 // indirect
-	k8s.io/client-go v0.19.0 // indirect
+	k8s.io/api v0.19.0
+	k8s.io/apimachinery v0.20.1
+	k8s.io/client-go v0.19.0
+	k8s.io/klog/v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -13,11 +13,12 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-var (
-	client = &kubernetes.Clientset{}
+const (
+	NAMESPACE = "awesome-o"
+)
 
-	//KUBECONFIG = "/home/karl/.kube/config"
-	KUBECONFIG = "/Users/karl/.kube/config"
+var (
+	KUBECONFIG = os.Getenv("HOME") + "/.kube/config"
 
 	POD = &v1.Pod{
 		TypeMeta: metav1.TypeMeta{
@@ -25,9 +26,9 @@ var (
 			APIVersion: "v1",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "nginx-pod",
-			Namespace: "default",
-			Labels:    map[string]string{
+			Name:      "nginx-awesome-pod",
+			Namespace: NAMESPACE,
+			Labels: map[string]string{
 				"run": "nginx-pod",
 			},
 		},
@@ -35,19 +36,17 @@ var (
 			Volumes: nil,
 			Containers: []v1.Container{
 				{
-					Name: "nginx-pod",
+					Name:  "nginx-pod-awesome",
 					Image: "nginx",
 				},
 			},
-			DNSPolicy: "ClusterFirst",
+			DNSPolicy:     "ClusterFirst",
 			RestartPolicy: "Always",
 		},
 	}
-
-	NAMESPACE = "default"
 )
 
-func clean() error {
+func clean(client *kubernetes.Clientset) error {
 	fmt.Println("clean invoked.")
 	// Check if pod exists.
 	old, _ := client.CoreV1().Pods(NAMESPACE).Get(context.TODO(), POD.Name, metav1.GetOptions{})
@@ -58,14 +57,32 @@ func clean() error {
 			// Report error.
 			fmt.Println("Error deleting pod")
 			return e
+		} else {
+			fmt.Println("pod was removed")
 		}
-		// return pod object.
-		return e
+	}
+	err := client.CoreV1().Namespaces().Delete(context.TODO(), NAMESPACE, metav1.DeleteOptions{})
+	if err != nil {
+		fmt.Println("error deleting namespace", err)
+	} else {
+		fmt.Println("namespace was removed")
 	}
 	return nil
 }
 
-func create() *v1.Pod{
+func create(client *kubernetes.Clientset) *v1.Pod {
+	// Check if namespace exists.
+	namespace, err := client.CoreV1().Namespaces().Get(context.TODO(), NAMESPACE, metav1.GetOptions{})
+	if err != nil {
+		namespace.Name = NAMESPACE
+		_, err := client.CoreV1().Namespaces().Create(context.TODO(), namespace, metav1.CreateOptions{})
+		if err != nil {
+			fmt.Println("cannot create namespace", err)
+			return &v1.Pod{}
+		} else {
+			fmt.Println("created namespace")
+		}
+	}
 	// Check if pod exists.
 	old, _ := client.CoreV1().Pods(NAMESPACE).Get(context.TODO(), POD.Name, metav1.GetOptions{})
 	if old.Name != POD.Name {
@@ -73,8 +90,10 @@ func create() *v1.Pod{
 		pod, err := client.CoreV1().Pods(NAMESPACE).Create(context.TODO(), POD, metav1.CreateOptions{})
 		if err != nil {
 			// Report error and return empty object.
-			fmt.Println("Error creating pod")
+			fmt.Println("Error creating pod", err)
 			return &v1.Pod{}
+		} else {
+			fmt.Println("Pod was created")
 		}
 		// return pod object.
 		return pod
@@ -82,7 +101,7 @@ func create() *v1.Pod{
 	return &v1.Pod{}
 }
 
-func update() *v1.Pod {
+func update(client *kubernetes.Clientset) *v1.Pod {
 	// Check if pod exists.
 	old, _ := client.CoreV1().Pods(NAMESPACE).Get(context.TODO(), POD.Name, metav1.GetOptions{})
 	if old.Name == POD.Name {
@@ -103,30 +122,27 @@ func main() {
 
 	config, _ := clientcmd.BuildConfigFromFlags("", KUBECONFIG)
 
-	client, _ = kubernetes.NewForConfig(config)
+	client, _ := kubernetes.NewForConfig(config)
+
+	fmt.Println("listening")
+
+	go func() {
+		for {
+
+			create(client)
+
+		}
+	}()
 
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt)
 
 	task := sync.WaitGroup{}
 	task.Add(1)
-	go func() {
-		defer clean()
-	}()
-
 	select {
 	case sig := <-c:
 		fmt.Printf("Got %s signal. Aborting...\n", sig)
+		clean(client)
 		task.Done()
 	}
-
-	fmt.Println("listening")
-
-	for {
-
-		create()
-
-	}
-	
-
 }
