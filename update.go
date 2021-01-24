67cfe5fa811dedd61b6d4f0d4814fc311cceb3f0
Karl Hepworth <karl.hepworth@gmail.com>
2021-01-24 22:21:24 +1100

    document broken-ness of update mechanism
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- update.go
+++ update.go
@@ -5,6 +5,9 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
+// TODO: This mechanism seems to be busted...
+//       Deleted objects are detected but not recreated.
+
 func (client *Application) UpdateAll() {
 	client.updateNamespace()
 	client.updateDeployment()
@@ -15,16 +18,15 @@ func (client *Application) updateNamespace() error {
 	if e := client.Client.Get(context.Background(), types.NamespacedName{
 		Namespace: namespaced.Namespace,
 		Name:      namespaced.Name,
-	}, namespaced); e == nil {
+	}, namespaced); e != nil {
+
+		// TODO: Problem lies here.
 		if err := client.Client.Update(context.Background(), namespaced); err != nil {
 			log(ns, "update", "namespace", false, err)
 			return err
 		} else {
 			log(ns, "update", "namespace", true, nil)
 		}
-	} else {
-		client.cleanNamespace()
-		client.createNamespace()
 	}
 	return nil
 }
@@ -33,16 +35,15 @@ func (client *Application) updateDeployment() error {
 	if e := client.Client.Get(context.Background(), types.NamespacedName{
 		Namespace: deployment.Namespace,
 		Name:      deployment.Name,
-	}, deployment); e == nil {
+	}, deployment); e != nil {
+
+		// TODO: Problem lies here.
 		if err := client.Client.Update(context.Background(), deployment); err != nil {
 			log(ns, "update", "deployment", false, err)
 			return err
 		} else {
 			log(ns, "update", "deployment", true, nil)
 		}
-	} else {
-		client.cleanDeployment()
-		client.createDeployment()
 	}
 	return nil
 }
@@ -51,16 +52,15 @@ func (client *Application) updateService() error {
 	if e := client.Client.Get(context.Background(), types.NamespacedName{
 		Namespace: service.Namespace,
 		Name:      service.Name,
-	}, deployment); e == nil {
+	}, deployment); e != nil {
+
+		// TODO: Problem lies here.
 		if err := client.Client.Update(context.Background(), service); err != nil {
 			log(ns, "update", "service", false, err)
 			return err
 		} else {
 			log(ns, "update", "service", true, nil)
 		}
-	} else {
-		client.cleanService()
-		client.createService()
 	}
 	return nil
 }
